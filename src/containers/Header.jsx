5a207d9a1bb4690a7ecea8a5e59d306929996f88
Yuri Mikushov <mikushov.yu@gmail.com>
2021-02-07 18:34:23 +0300

    feat: added full name and position to store

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { observer } from 'mobx-react-lite'
 import Header from '../components/Header'
+import { useAboutMeStore } from '../stores'
 
-const HeaderContainer = () => (
-  <Header name='Full name' position='Frontend developer' />
-)
+const HeaderContainer = observer(() => {
+  const aboutMeStore = useAboutMeStore()
+
+  return (
+    <Header name={aboutMeStore.fullName} position={aboutMeStore.position} />
+  )
+})
 
 export default HeaderContainer
--- src/stores/aboutMeStore.js
+++ src/stores/aboutMeStore.js
@@ -2,7 +2,15 @@ const exapmleDescription =
   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto laborum dolores eos deleniti inventore delectus omnis, velit repudiandae qui nobis vel consequatur quisquam. Doloremque incidunt magnam quasi autem mollitia est praesentium voluptate voluptatem, harum, similique temporibus architecto eum cupiditate eligendi enim ab iste, alias ea. Veritatis id harum dolores unde.'
 
 const createAboutMeStore = () => ({
+  fullName: 'Full name',
+  position: 'Frontend developer',
   description: exapmleDescription,
+  setFullName(fullName) {
+    this.fullName = fullName
+  },
+  setPosition(position) {
+    this.position = position
+  },
   setAboutMe(description) {
     this.description = description
   },
