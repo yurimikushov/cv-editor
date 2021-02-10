81ac1b16a5bb31d1764923b876d8cdb8b0fe6b0c
Yuri Mikushov <mikushov.yu@gmail.com>
2021-02-10 08:55:30 +0300

    fix: used textarea for multiline text

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -4,13 +4,16 @@ import './index.css'
 
 const AboutMe = ({ showPreview, description, setDescription }) => (
   <div className='about-me item item_1'>
-    <div
-      className='about-me__description'
-      contentEditable={!showPreview}
-      onBlur={(e) => setDescription(e.target.innerText)}
-    >
-      {description}
-    </div>
+    {showPreview ? (
+      <div className='about-me__description'>{description}</div>
+    ) : (
+      <textarea
+        className='about-me__description'
+        defaultValue={description}
+        placeholder='Describe yourself'
+        onChange={(e) => setDescription(e.target.value)}
+      ></textarea>
+    )}
   </div>
 )
 
