a126aac3cfcfb982ceaadb16ce47f1e3af8f2401
Yuri Mikushov <mikushov.yu@gmail.com>
2021-02-18 08:37:32 +0300

    refactor: extracted css import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import StoreProvider from './store'
 import App from './App'
-import './reset.css'
 import './index.css'
 
 ReactDOM.render(
