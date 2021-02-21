929b109fa6e54f207a035314dba912d216580e06
Yuri Mikushov <mikushov.yu@gmail.com>
2021-02-21 11:47:39 +0300

    chore: added download pdf util

---
+++ src/utils/download-pdf.js
@@ -0,0 +1,13 @@
+import html2pdf from 'html2pdf.js'
+
+const downloadPDF = (filename, fromSelecotor) => {
+  html2pdf()
+    .set({
+      filename: filename,
+      margin: 20,
+    })
+    .from(document.querySelector(fromSelecotor))
+    .save()
+}
+
+export default downloadPDF
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export { default as downloadPDF } from './download-pdf'
