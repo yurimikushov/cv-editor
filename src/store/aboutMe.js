f0554f4a4f0828f69399bb84914d263d96982803
Yuri Mikushov <mikushov.yu@gmail.com>
2021-02-21 11:17:19 +0300

    chore: compressed pic of avatar example

--- src/store/aboutMe.js
+++ src/store/aboutMe.js
@@ -18,7 +18,7 @@ const createAboutMe = () => ({
   fillInExample() {
     this.setFullName('Yuri Mikushov')
     this.setPosition('Frontend develover')
-    this.setAvatar('/img/avatar-example.png')
+    this.setAvatar('/img/avatar-example-100x100.png')
     this.setDescription(
       `I've been developing and supporting a software products running on the 1C:Enterprise platform for about 3 years. I've a lot of interest in creating and programming an user interfaces. Now I'm trying to start developing a frontend apps professionally.`
     )
