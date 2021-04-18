d946848a67812d261693c1f4b27ae75fedb0cef3
a <ya.mikushoff@gmail.com>
2021-04-18 23:39:05 +0300

    implement github sign-in

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, createContext, useReducer } from 'react'
 import { observer } from 'mobx-react-lite'
 import {
   useUpdateAppTitle,
@@ -10,18 +10,41 @@ import Loader from './components/Common/Loader'
 import Toolbar from './components/Toolbar'
 import CV from './pages/CV'
 import './App.css'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { initialState, reducer } from "./store/reducer";
+
+export const AuthContext = createContext();
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useUpdateAppTitle()
   useUpdateHtmlLangAttr()
   useLoadAppData()
   useSaveAppData()
 
   return (
-    <Suspense fallback={<Loader className='app-loader' />}>
-      <Toolbar />
-      <CV />
-    </Suspense>
+      <div>
+        <AuthContext.Provider
+            value={{
+              state,
+              dispatch
+            }}
+        >
+          <Router>
+            <Switch>
+              <Route path="/login" component={Login}/>
+              <Route path="/" component={Home}/>
+            </Switch>
+          </Router>
+        </AuthContext.Provider>
+        <Suspense fallback={<Loader className='app-loader' />}>
+          <Toolbar />
+          <CV />
+        </Suspense>
+      </div>
   )
 }
 
